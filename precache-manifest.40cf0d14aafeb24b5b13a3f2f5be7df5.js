3bd44e50a29ba2b67e9ef30c1d109430549f46f9
igaryok <igor.sobko@gmail.com>
2019-09-18 13:03:12 +0300

    Updates

---
+++ precache-manifest.40cf0d14aafeb24b5b13a3f2f5be7df5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a168ee4cd0468cca3890f68d59a1ea3",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "1fd8149ef3bdf19ff9d4",
+    "url": "/react_add-todo-form/static/css/main.9c9a97b2.chunk.css"
+  },
+  {
+    "revision": "c4f9d12e475d3c2c3191",
+    "url": "/react_add-todo-form/static/js/2.54a8cf50.chunk.js"
+  },
+  {
+    "revision": "1fd8149ef3bdf19ff9d4",
+    "url": "/react_add-todo-form/static/js/main.de6786e9.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
